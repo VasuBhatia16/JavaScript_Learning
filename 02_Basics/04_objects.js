95e93f17a6942056dd8d0cd7243058151da85985
Vasu Bhatia <vasubhatia@dldavppcbsei.com>
2024-02-14 13:46:55 +0530

    Added Objects Part -2

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,57 @@
+//Part - 2 => Objects
+
+//Objects with constructor  => Singleton
+
+// const tinderUser = new Object()   === {} //Empty object
+const tinderUser = {}
+tinderUser.id="121344";
+tinderUser.name="javaScript"
+tinderUser.isLoggedIn = false;
+
+const regularUser = {
+    email:"name@gmail.com",
+    fullname: {
+        userfullname:{
+            firstname:"Vasu",
+            lastname:"Bhatia"
+        }
+    }
+}
+
+const obj1 = {
+    1:"a",2:"b"
+}
+const obj2 = {
+    3:"a",4:"b"
+}
+let obj3 = Object.assign({}, obj1,obj2); //first parameter = {} (target) If not given then target will be obj1 
+obj3 = {...obj1,...obj2}
+// const obj3  = {obj1,obj2}//Nested Array gets created
+console.log(obj3);
+console.log(regularUser.fullname.userfullname.firstname);  
+console.log(tinderUser);
+
+
+const users = [
+    {
+        id:1,
+        email: "h@gmail.com"
+    },
+    {
+        id:2,
+        email: "g@gmail.com"
+    },
+    {
+        id:2,
+        email: "f@gmail.com"
+    }
+] //Array of objects
+
+console.log(users[1]["id"]);
+
+console.log(Object.keys(tinderUser)); //Array of keys
+console.log(Object.values(tinderUser)); //Array of values
+console.log(Object.entries(tinderUser));//Array of array of each key value pair
+
+console.log(tinderUser.hasOwnProperty('isLogged'));//False
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));//True
